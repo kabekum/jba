a98ba3a26862c8b9bf5d6bd9faa712477114fbd9
kabekum <98370249+kabekum@users.noreply.github.com>
2025-12-16 03:06:39 -0800

    www7

--- jura_backend/settings.py
+++ jura_backend/settings.py
@@ -81,4 +81,4 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS','jba-cq78.onrender.com').split(',')
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS','https://jba-cq78.onrender.com').split(',')
